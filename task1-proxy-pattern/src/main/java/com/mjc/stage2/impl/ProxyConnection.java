37f33ae58916998520625168200318e099015958
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-23 14:33:34 +0300

    Fixed object in close() method

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -16,12 +16,12 @@ public class ProxyConnection implements Connection {
 
     @Override
     public void close() {
-
-        ConnectionPool.getInstance().releaseConnection(realConnection);
+        ConnectionPool.getInstance().releaseConnection(this);
     }
 
     @Override
     public boolean isClosed() {
         return false;
     }
+
 }
